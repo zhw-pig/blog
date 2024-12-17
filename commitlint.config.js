5ab4a997079c7a33163d43eb6d4122623ca96b6c
张怀伟 <zhanghuaiwei@southzn.com>
2024-12-17 17:19:54 +0800

    Feat: 项目添加commit、stylelint校验

---
+++ commitlint.config.js
@@ -0,0 +1,40 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				// 编译相关的修改，例如发布版本，对项目构建或者依赖的改动
+				'Build',
+				// 新功能(feature)
+				'Feat',
+				// 修复bug
+				'Fix',
+				// 更新某功能
+				'Update',
+				// 重构
+				'Refactor',
+				// 文档
+				'Docs',
+				// 构建过程或者辅助工具的变动,如增加依赖库等
+				'Chore',
+				// 不影响代码运行的变动
+				'Style',
+				// 撤销commit,回滚到上一个版本
+				'Revert',
+				// 性能优化
+				'Perf',
+				// 测试(单元,集成测试)
+				'Test',
+			],
+		],
+		'type-case': [0],
+		'type-empty': [0],
+		'scope-empty': [0],
+		'scope-case': [0],
+		'subject-full-stop': [0, 'never'],
+		'subject-case': [0, 'never'],
+		'header-max-length': [0, 'always', 74],
+	},
+};
